a97f35263cea4e258bafd09ea9ed65e965e3e1e6
Eduardo Jair Hernández Gómez <88729982+LaloHGz@users.noreply.github.com>
2024-03-09 16:48:05 -0600

    Initial commit

---
+++ VerdeLunar/VerdeLunarApp.swift
@@ -0,0 +1,17 @@
+//
+//  VerdeLunarApp.swift
+//  VerdeLunar
+//
+//  Created by Alumno on 09/03/24.
+//
+
+import SwiftUI
+
+@main
+struct VerdeLunarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

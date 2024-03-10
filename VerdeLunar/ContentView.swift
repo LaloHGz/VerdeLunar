d6e7ed5d529634cf29005bd78ccfdb81331ced18
Eduardo Jair Hernández Gómez <88729982+LaloHGz@users.noreply.github.com>
2024-03-09 18:40:18 -0600

    Zstack top
    set  zstack to the top

--- VerdeLunar/ContentView.swift
+++ VerdeLunar/ContentView.swift
@@ -9,13 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+            ZStack{
+                Color.red
+                    .aspectRatio(contentMode: .fit)
+                    .opacity(0.8)
+                    .ignoresSafeArea(.all)
+                    /*.resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .opacity(0.8)
+                    .edgesIgnoringSafeArea(.all)
+                     */
+                VStack {
+                    Image("toalla-tela")
+                        .resizable()
+                        .frame(width: 200, height: 200)
+                        .padding(.bottom, 10)
+                    Text("¿Cómo crear una Toalla de Tela?")
+                        .foregroundStyle(.white)
+                    
+                }
+                    
+                
+                
+            }
+            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
     }
 }
 

0870c2daa4827f11ca3e5f5d27696dcc1e536f8e
“abeejita” <74677482+carminalp@users.noreply.github.com>
2024-03-10 06:18:58 -0600

    Intento 1

--- VerdeLunar/verdeLunar/ContentView.swift
+++ VerdeLunar/verdeLunar/ContentView.swift
@@ -7,38 +7,106 @@
 
 import SwiftUI
 import RealityKit
+import ARKit
 
 struct ContentView : View {
+    @State private var isPlacementEnabled = false
+    @State private var selectedModel: String?
+    @State private var modelConfirmed: String?
+    
+    
+    var models: [String] = ["toalla1","toalla2"]
+    
     var body: some View {
-        ARViewContainer().edgesIgnoringSafeArea(.all)
+        ZStack(alignment: .bottom){
+            ARViewContainer()
+            // visualiza el picker
+            PlacementButtonsView(isPlacementEnabled: self.$isPlacementEnabled,  selectedModel: self.$selectedModel, modelConfirmed: self.$modelConfirmed)
+        }
     }
 }
 
+
 struct ARViewContainer: UIViewRepresentable {
+  
+        func makeUIView(context: Context) -> ARView {
+         
+         let arView = ARView(frame: .zero)
+         
+         let config = ARWorldTrackingConfiguration()
+         config.planeDetection = [.horizontal, .vertical]
+         config.environmentTexturing = .automatic
+         
+         if ARWorldTrackingConfiguration.supportsSceneReconstruction(.mesh) {
+         config.sceneReconstruction = .mesh
+         }
+         
+         arView.session.run(config)
+         
+         return arView
+         
+         }
+         
+         func updateUIView(_ uiView: ARView, context: Context) {
+             let fileName = "toalla.usdz"
+             
+             print("DEBUG: adding model to scene \(fileName)")
+             
+             let modelEntity = try! ModelEntity.loadModel(named: fileName)
+             
+             let anchorEntity = AnchorEntity(plane: .any)
+             anchorEntity.addChild(modelEntity)
+             
+             uiView.scene.addAnchor(anchorEntity)
+         }
+
+}
+
+
+struct PlacementButtonsView: View {
+    @Binding var isPlacementEnabled: Bool
+    @Binding var selectedModel:String?
+    @Binding var modelConfirmed: String?
     
-    func makeUIView(context: Context) -> ARView {
-        
-        let arView = ARView(frame: .zero)
-
-        // Create a cube model
-        let mesh = MeshResource.generateBox(size: 0.1, cornerRadius: 0.005)
-        let material = SimpleMaterial(color: .gray, roughness: 0.15, isMetallic: true)
-        let model = ModelEntity(mesh: mesh, materials: [material])
-        model.transform.translation.y = 0.05
-
-        // Create horizontal plane anchor for the content
-        let anchor = AnchorEntity(.plane(.horizontal, classification: .any, minimumBounds: SIMD2<Float>(0.2, 0.2)))
-        anchor.children.append(model)
-
-        // Add the horizontal plane anchor to the scene
-        arView.scene.anchors.append(anchor)
-
-        return arView
-        
+    var body: some View {
+        HStack {
+            // CANCEL BUTTON
+            Button(action: {
+                print("DEBUG: Cancel Model ")
+                
+                self.resetPlacementParameters()
+            }){
+                Image(systemName: "xmark")
+                    .frame(width: 60, height: 60)
+                    .font(.title)
+                    .background(Color.white
+                        .opacity(0.75))
+                    .cornerRadius(30)
+                    .padding(20)
+            }
+            // CONFIRM BUTTON
+            Button(action: {
+                print("DEBUG: Confirmed Model")
+                self.modelConfirmed = self.selectedModel
+            }){
+                Image(systemName: "checkmark")
+                    .frame(width: 60, height: 60)
+                    .font(.title)
+                    .background(Color.white
+                        .opacity(0.75))
+                    .cornerRadius(30)
+                    .padding(20)
+            }
+        }
+    }
+    func resetPlacementParameters() {
+        self.isPlacementEnabled = false
+        self.selectedModel = nil
     }
     
-    func updateUIView(_ uiView: ARView, context: Context) {}
-    
+    func back() {
+        //Mandar a la pantalla anterior
+    }
 }
 
 #Preview {
